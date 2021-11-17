90c9c1221661d64b7fad427c8dfbffd0661e3fec
bkern98 <bkern288@gmail.com>
2021-11-17 14:23:09 -0500

    Modified landing page

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,37 +1,40 @@
 import * as React from 'react';
-import { Grid, Typography, Container, Box } from '@mui/material';
+import { Grid, Typography, Container, Box, Button } from '@mui/material';
 import Link from '../src/Link';
 import Copyright from '../src/Copyright';
 
 export default function Index() {
   return (
-    <Container maxWidth='sm'>
-      <Box sx={{ my: 4 }}>
-        <Grid container sx={{ alignItems: 'center' }}>
+    <Container maxWidth='m'>
+      <Box sx={{ my: 4, alignContent: 'center' }}>
+        <Grid container>
           <Grid item xs={12}>
-            <Typography
-              variant='h4'
-              component='h1'
-              gutterBottom
-              textAlign='center'
-            >
-              Welcome to Readit!
+            <Typography variant='h1' sx={{ fontSize: '25vw' }}>
+              Readit.
             </Typography>
           </Grid>
-          <Grid item xs={1}></Grid>
-          <Grid item xs={7}>
-            <Typography>Returning User?</Typography>
-            <Link href='/login' color='secondary'>
-              Login
-            </Link>
+          <Grid item xs={12}>
+            <Typography
+              variant='h5'
+              gutterBottom
+              sx={{ ml: '0.8em', pb: '0.8em' }}
+            >
+              A Place for Readers to Discuss Their Favorite Books
+            </Typography>
           </Grid>
-          <br />
-          <Grid item>
-            <Typography>First Time Here?</Typography>
-            <Link href='/signup' color='secondary'>
-              Sign Up
-            </Link>
+          <Grid item xs={12} sx={{ ml: '1em' }}>
+            <Typography>
+              <Button variant='outlined' href='/signup'>
+                Create An Account
+              </Button>{' '}
+              or{' '}
+              <Button variant='outlined' href='/login'>
+                Login Here
+              </Button>
+            </Typography>
           </Grid>
+          <Grid xs={1}></Grid>
+          <Grid item></Grid>
         </Grid>
       </Box>
     </Container>
